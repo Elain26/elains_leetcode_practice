b2a4aa935611c27c19b59bc43e8d822bbaf37d33
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-09-03 23:21:11 +0900

    Update 8Check If N and Its Double Exist.py

--- 8Check If N and Its Double Exist.py
+++ 8Check If N and Its Double Exist.py
@@ -1,6 +1,7 @@
 arr=[10,2,5,3]
 def doubleornot(arr):
     n=len(arr)
+    #暴力穷举对比任意两个数是否有两倍关系，注意两数不能同为0
     for i in range(n):
         for j in range(n):
             if arr[i]*2==arr[j] and i!=j:
