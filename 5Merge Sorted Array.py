1f66c6af66ce2f50d0d363f6f44a7f2c5da2d26c
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-09-03 23:28:43 +0900

    Update 5Merge Sorted Array.py

--- 5Merge Sorted Array.py
+++ 5Merge Sorted Array.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
+        #这题怎么不许我用python的sort哼唧！
+        #还好两个数列是排好序的，数列一和数列二从最后一个数开始比较，大的那个丢到数列一的尾巴去
+        #就这样一个一个数从后往前填
         while m>0 and n>0:
             if nums1[m-1]<nums2[n-1]:
                 nums1[m+n-1]=nums2[n-1]
@@ -7,6 +10,8 @@ class Solution(object):
             else:
                 nums1[m+n-1]=nums1[m-1]
                 m=m-1
+        #如果数列一的数已经填完了但是数列二还剩，即n>0
+        #由于是已经排好序的数列，只需将数列二剩的数填进数列一前面
         if n>0:
             nums1[:n]=nums2[:n]
                 
