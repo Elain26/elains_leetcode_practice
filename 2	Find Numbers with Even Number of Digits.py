65f40fa68fe1119a8b436593b7f9c4135b14f23e
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-09-03 23:32:26 +0900

    Update 2	Find Numbers with Even Number of Digits.py

--- 2	Find Numbers with Even Number of Digits.py
+++ 2	Find Numbers with Even Number of Digits.py
@@ -3,6 +3,7 @@ class Solution(object):
         result=0
         n=len(nums)
         for i in range(0,n):
+            #将数字强行转成string然后用len得到string长度，python赛高！
             num_len=len(str(nums[i]))
             if (num_len%2)==0:
                 result=result+1
