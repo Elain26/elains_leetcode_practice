96e875697c9f9315e9ad3cf42e709e16a8c18de8
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-09-03 23:34:56 +0900

    Update 1Max Consecutive Ones.py

--- 1Max Consecutive Ones.py
+++ 1Max Consecutive Ones.py
@@ -1,13 +1,16 @@
 class Solution(object):
+    #找最多连续的数字1（测试数列只有0和1）
     def findMaxConsecutiveOnes(self, nums):
         n=len(nums)
         size=0
         maximunsize=0
-        for i in range(0,n):         
+        for i in range(0,n): 
+            #最多连续数字1计数，然后存在maximunsize里
             if nums[i]==1:
                 size=size+1
                 if size>maximunsize:                   
                     maximunsize=size
+            #遇到0就重置计数
             if nums[i]==0:                
                 size=0           
         return maximunsize
