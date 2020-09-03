87908c70dd4b9a1d63014025b5f85e4441495042
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-09-03 23:23:50 +0900

    Update 6Remove Element.py

--- 6Remove Element.py
+++ 6Remove Element.py
@@ -1,7 +1,10 @@
 nums = [3,2,2,3]
 value=2
 while value in nums:
+    #找目标数位置
     position=nums.index(value)
+    #把数列最后一个数放到原目标数位置
     nums[position]=nums[-1]
+    #然后删掉数列最后一个数
     nums.pop()
 print(nums)
