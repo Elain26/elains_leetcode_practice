8f933a947acc650baff4aeb966d57b012729e0f7
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-09-24 15:59:10 +0900

    Update 19Palindrome Linked List.py

--- 19Palindrome Linked List.py
+++ 19Palindrome Linked List.py
@@ -1,1 +1,13 @@
 
+Node1=ListNode(1)
+Node2=ListNode(2)
+Node3=ListNode(3)
+Node4=ListNode(4)
+Node5=ListNode(5)
+Node1.next=Node2
+Node2.next=Node3
+Node3.next=Node4
+Node4.next=Node5
+#check if the middle Node is correct
+a=Solution()
+ans=a.reverseList(Node1)
