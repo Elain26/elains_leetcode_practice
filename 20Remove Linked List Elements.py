c777942b9713d8e1677f0007a1176fa85db39056
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-09-24 18:47:18 +0900

    Update 20Remove Linked List Elements.py

--- 20Remove Linked List Elements.py
+++ 20Remove Linked List Elements.py
@@ -3,33 +3,28 @@ class ListNode(object):
         self.val = val
         self.next = next
 class Solution(object):
-    def isPalindrome(self, head):
-      #新建空列表
-        list1=[]
-      #搞个指针记一下头
-        p=head
-      #把链表里的值都记录进列表里
-        while head:
-            list1.append(head.val)
-            head=head.next
-        print(list1)
-       #把列表里最后一个数和链表里第一个数开始一个一个比对
-        l=int(len(list1)/2)
-        for i in range(l):
-            a=list1.pop()
-            if a==p.val:
-                p=p.next
+    def removeElements(self, head, val):
+        #给一个空的头
+        p0=ListNode(0)
+        #这个头接原来的head
+        p0.next=head
+        #指针指向head
+        p1=p0
+        #当指针和指针下一个都有的话
+        while p1 and p1.next:                      
+            if p1.next.val==val:
+                p1.next=p1.next.next
             else:
-                return False
-        return True
-        
+                p1=p1.next
+        return p0.next
+
 Node1=ListNode(1)
-Node2=ListNode(2)
-Node3=ListNode(2)
-Node4=ListNode(1)
+"""Node2=ListNode(2)
+Node3=ListNode(3)
+Node4=ListNode(4)
 Node1.next=Node2
 Node2.next=Node3
-Node3.next=Node4
+Node3.next=Node4"""
 a=Solution()
-ans=a.isPalindrome(Node1)
+ans=a.removeElements(Node1, 1)
 print(ans)
