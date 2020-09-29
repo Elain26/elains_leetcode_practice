9688dd285411eb5b7fdea4488482e46a75bf19d0
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-09-29 14:51:40 +0900

    Create 23Remove Duplicates from Sorted List.py

---
+++ 23Remove Duplicates from Sorted List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        #record the beginning
+        p=head
+        while head and head.next:
+            if head.val==head.next.val:
+                head.next=head.next.next
+                #if this happen, jump out of the while circle
+                continue
+            head=head.next
+        return p
