68ee9629808507552f3f4a113be1b1d7550112b1
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-10-05 14:15:42 +0900

    Create 29Odd Even Linked List.py

---
+++ 29Odd Even Linked List.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        odd=head
+        even=head.next
+        while odd:
+            odd.next=odd.next.next
+            odd=odd.next
+            
