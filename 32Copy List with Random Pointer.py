b0b639033ac7d64eacbe1d8a49e1dc588f0b0ee8
Elain26 <58205424+Elain26@users.noreply.github.com>
2020-10-06 17:05:00 +0900

    Create 32Copy List with Random Pointer.py

---
+++ 32Copy List with Random Pointer.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        if head is None:
+            return None
+        copyList=dict()
+        node=head
+        while node:
+            copyList[Node]=Node(node.val)
+            node=node.next
